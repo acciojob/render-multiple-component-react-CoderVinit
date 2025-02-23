098d2990314e9997de6e0c75fa9fd297d5bbaf21
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-23 09:28:19 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Project from "./Project";
+
+
+const projects = [
+  { name: "ChatApp", description: "A real-time chat application using Socket.io." },
+  { name: "Doctor Appointment App", description: "An app for booking and managing doctor appointments." },
+  { name: "Shopping Cart", description: "An e-commerce shopping cart with Razorpay integration." }
+];
 
 const App = () => {
+
   return (
-    <div id="main"></div>
+    <div id="ns-wrapper">
+      {
+        projects?.map((project,index)=>(
+          <Project key={index} name={project.name} description={project.description}/>
+        ))
+      }
+    </div>
   )
 }
 
---
+++ src/components/Project.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+const Project = ({name,description}) => {
+  return (
+    <div className='project-card'>
+        <h1 data-ns-test="project-name">{name}</h1>
+        <h6 data-ns-test="project-description">{description}</h6>
+    </div>
+  )
+}
+
+export default Project
